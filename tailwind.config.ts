84f868424391b2899df4ea4833d6b4df2f33669e
ecxtacy <dc.dhruvchouhan@gmail.com>
2024-01-15 15:39:45 +0530

    Configured MDX with styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -15,6 +16,7 @@ const config: Config = {
       colors: {
         trans: {
           blue: "rgba(11, 78, 172, 0.45)", 
+          cyan: "rgba(175, 217, 231, 0.59)",
         }
       }
     },
